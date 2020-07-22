e73f4b23f58bf5cb79fea9ef6ec0770950ce02cb
Dominique <cyanthecomputerscientist@gmail.com>
2020-07-22 12:31:51 -0400

    Testing works

--- src/main/java/com/codedifferently/wordcounter/WordCounter.java
+++ src/main/java/com/codedifferently/wordcounter/WordCounter.java
@@ -1,4 +1,8 @@
 package com.codedifferently.wordcounter;
 
+import java.util.HashMap;
+
 public class WordCounter {
+
+    HashMap<String , Integer> hashMap = new HashMap<String,Integer>();
 }
--- src/test/java/com/codedifferently/wordcounter/WordCounterTest.java
+++ src/test/java/com/codedifferently/wordcounter/WordCounterTest.java
@@ -1,4 +1,12 @@
 package com.codedifferently.wordcounter;
 
+import org.junit.Test;
+
 public class WordCounterTest {
+
+    @Test
+    public void test()
+    {
+
+    }
 }
